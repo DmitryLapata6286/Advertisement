eefc4f5a08b2424c518b303d8827d3dc2363b0bc
DmitryLapata6286 <dmitry.lapata@gmail.com>
2022-07-04 18:30:26 +0300

    feat: created UI items, wrote model

---
+++ Adv/Extensions/UIView+Ex.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Ex.swift
+//  Adv
+//
+//  Created by Dmitry Lapata on 3.07.22.
+//
+
+import Foundation
---
+++ Adv/Models/Advertisement.swift
@@ -0,0 +1,8 @@
+//
+//  Advertisement.swift
+//  Adv
+//
+//  Created by Dmitry Lapata on 4.07.22.
+//
+
+import Foundation
